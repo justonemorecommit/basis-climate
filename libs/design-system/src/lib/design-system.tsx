42851d45c1d9660dae72ddf0b59173df18427803
Jackson <stayhungryjack@gmail.com>
2023-03-12 09:19:41 -0500

    Added a lib for design system

---
+++ libs/design-system/src/lib/design-system.tsx
@@ -0,0 +1,32 @@
+
+
+
+
+
+  import styled from '@emotion/styled';
+  
+
+/* eslint-disable-next-line */
+export interface DesignSystemProps {
+}
+
+
+const StyledDesignSystem = styled.div`
+  color: pink;
+`;
+
+
+export function DesignSystem(props: DesignSystemProps) {
+  return (
+    <StyledDesignSystem>
+      
+      <h1>Welcome to DesignSystem!</h1>
+      
+    </StyledDesignSystem>
+  );
+};
+
+
+export default DesignSystem;
+
+
