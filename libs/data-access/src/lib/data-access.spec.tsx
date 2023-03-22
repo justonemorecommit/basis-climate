b504e80eed56c5ad619a0fbc6a82c8bbdcbf3cb4
Jackson <stayhungryjack@gmail.com>
2023-03-21 20:39:52 -0500

    Generated data-access package

---
+++ libs/data-access/src/lib/data-access.spec.tsx
@@ -0,0 +1,12 @@
+import { render } from '@testing-library/react';
+
+import DataAccess from './data-access';
+
+describe('DataAccess', () => {
+  
+  it('should render successfully', () => {
+    const { baseElement } = render(<DataAccess />);
+    expect(baseElement).toBeTruthy();
+  });
+  
+});
