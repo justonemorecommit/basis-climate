e9cc57ce2ab22ae9783b456e3f1c23d4c46d613c
Jackson <stayhungryjack@gmail.com>
2023-03-23 14:37:46 -0500

    Typing for environment

---
+++ apps/api/src/@typings/environment.d.ts
@@ -0,0 +1,13 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    DATABASE_URL: string;
+    NX_API_URL: string;
+    NX_AUTH0_DOMAIN: string;
+    NX_AUTH0_CLIENT_ID: string;
+    NX_AUTH0_CLIENT_SECRET: string;
+    NX_AUTH0_AUDIENCE: string;
+    NX_APP_URL: string;
+    NX_AUTO_NGROK: string;
+    NX_APP_SECRET: string;
+  }
+}
